7da2c1ab9673264da39518589f404146e906d19c
stwallity <wallition15@gmail.com>
2022-10-21 17:53:10 +0300

    layout.js - changed return in arrow function layout

--- blognjs/components/layout.js
+++ blognjs/components/layout.js
@@ -1,5 +1,15 @@
-const layout = () => {
-  return <div>Enter</div>;
+const layout = ({
+  title = "Blog on Next.js - Simple Version",
+  description = "",
+}) => {
+  return;
+  <Head>
+    <title>{title} || ''</title>
+    <meta>name="description" content={description || "Blog on Next.js"}</meta>
+    <meta>
+      name='og:description' content={description || "Blog on Next.js"}
+    </meta>
+  </Head>;
 };
 
 export default layout;
