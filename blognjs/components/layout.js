e20371870b3417a68f3bf6379822f3097692acc4
stwallity <wallition15@gmail.com>
2022-10-21 18:17:01 +0300

    Layout.js : added footer, header

--- blognjs/components/layout.js
+++ blognjs/components/layout.js
@@ -1,16 +1,32 @@
+import Head from "next/head";
 const layout = ({
   title = "Blog on Next.js - Simple Version",
   description = "",
+  children,
 }) => {
-  return;
-  <Head>
-    <title>{title} || ''</title>
-    <meta>name="description" content={description || "Blog on Next.js"}</meta>
-    <meta>
-      name='og:description' content={description || "Blog on Next.js"}
-    </meta>
-  </Head>;
+  return (
+    <>
+      <Head>
+        <title>{title} || ''</title>
+
+        <meta>
+          name="description" content={description || "Blog on Next.js"}
+        </meta>
+        <meta>
+          name='og:description' content={description || "Blog on Next.js"}
+        </meta>
+      </Head>
+      <header>
+        <img src="/share-post.png" alt="Reload the page please"></img>
+        <Link href="/">
+          <a>Главная</a>
+        </Link>
+      </header>
+      <div class="container">{children}</div>
+      <footer>2022 &copy; Blog</footer>
+    </>
+  );
 };
 
-export default layout;
+export default Layout;
 // adding <header></header> and <footer></footer>
