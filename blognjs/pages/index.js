288d9971fa11a02d9a7f7339d55da7db503e7f6c
stwallity <wallition15@gmail.com>
2022-10-21 20:12:30 +0300

    index.js, layout.jsx changed

--- blognjs/pages/index.js
+++ blognjs/pages/index.js
@@ -1,7 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/layout";
 
 export default function Home() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <h1>Интересный блог</h1>
+    </Layout>
+  );
 }
