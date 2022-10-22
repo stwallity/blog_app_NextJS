4a3240a8f8ff26634f5a8d6b8eb4870b79c5970b
stwallity <wallition15@gmail.com>
2022-10-22 18:27:07 +0300

    index.js: getting posts from jsonplaceholder via fetch requests.

--- blognjs/components/Layout.jsx
+++ blognjs/components/Layout.jsx
@@ -15,11 +15,12 @@ const layout = ({
           type="image/png"
         ></link>
       </Head>
+
       <header>
-        <img src="/share-post.png" alt="Reload the page please"></img>
         <Link href="/">
           <a>Главная</a>
         </Link>
+        <img src="/share-post.png" alt="Reload the page please"></img>
       </header>
       <div class="container">{children}</div>
       <footer>2022 &copy; Blog</footer>
--- blognjs/pages/index.js
+++ blognjs/pages/index.js
@@ -1,9 +1,33 @@
+import BlogItem from "../components/BlogItem";
 import Layout from "../components/layout";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <Layout>
       <h1>Интересный блог</h1>
+      <div class="posts">
+        {posts.map((post) => {
+          <BlogItem
+            title={post.title}
+            description={post.description}
+            id={post.id}
+          ></BlogItem>;
+        })}
+      </div>
     </Layout>
   );
 }
+
+//taking from server
+//server requests
+//props -> destr -> posts
+//fetch from jsonplaceholder
+export const getServerSideProps = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  const posts = await data.json();
+  return {
+    props: { posts },
+  };
+};
